fcd9a04a9e32805ba99cd7c24ad6b4ba7688dcf0
zyvsagg <iamskriller@gmail.com>
2024-01-25 15:54:24 +0545

    Add - news item component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description,image } = this.props;
     return (
       <div>
-this is a news components
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={image}
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsItems" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
