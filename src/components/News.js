732c63a34e989d7819daacad801f0949c974b6cf
zyvsagg <iamskriller@gmail.com>
2024-01-25 15:55:41 +0545

    Add - news page

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,31 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        this is news section
-        <NewsItem/>
+      <div className='container my-4 '>
+        <h2>NewsMonkey - Top Headline</h2>
+        <div className="row">
+          <div className="col-md-4">
+          <NewsItem title="mo idiot" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="myTitle" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="myTitle" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+          <NewsItem title="Mo subash lodu ho" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="myTitle" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="myTitle" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
+          </div>
+        </div>  
+        
       </div>
     )
   }
