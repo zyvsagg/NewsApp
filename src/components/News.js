be3fd03951cb543a7a801f3a86d9df38d97c47e2
zyvsagg <iamskriller@gmail.com>
2024-01-26 14:53:36 +0545

    Add: News api

--- src/components/News.js
+++ src/components/News.js
@@ -2,32 +2,40 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+ 
+  constructor(){
+    super();
+    console.log("hellooooo im from news components");
+    this.state = {
+     articles: [],
+     loading: false
+    }
+      }
+
+
+     async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0b5d88a4262d41e9a7af59ac51563f84";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles})
+      }
+
   render() {
     return (
       <div className='container my-4 '>
-        <h2>NewsMonkey - Top Headline</h2>
+        <h1>NewsMonkey - Top Headline</h1>
+        
         <div className="row">
-          <div className="col-md-4">
-          <NewsItem title="mo idiot" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
+        {this.state.articles.map((element)=>{
+         return <div className="col-md-4 " key ={element.url}>
+          <NewsItem  title={element.title}description={element.description} image={element.urlToImage} newsUrl={element.url}/>
           </div>
-          <div className="col-md-4">
-          <NewsItem title="myTitle" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-          </div>
-          <div className="col-md-4">
-          <NewsItem title="myTitle" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-          </div>
+          
+        })}
+          
         </div>
-        <div className="row">
-          <div className="col-md-4">
-          <NewsItem title="Mo subash lodu ho" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-          </div>
-          <div className="col-md-4">
-          <NewsItem title="myTitle" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-          </div>
-          <div className="col-md-4">
-          <NewsItem title="myTitle" description="mydesc" image="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-          </div>
-        </div>  
+        
         
       </div>
     )
