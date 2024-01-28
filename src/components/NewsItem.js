94aa59286b15ce8fdf7495852d8bf7ae146fa321
zyvsagg <iamskriller@gmail.com>
2024-01-28 14:11:33 +0545

    Add: button color

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -16,7 +16,7 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+            <a rel="noreferreer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
               Read More 
             </a>
           </div>
