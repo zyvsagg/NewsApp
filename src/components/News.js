9f78c3df53a132b09fc39ff31e88097326f6059b
zyvsagg <iamskriller@gmail.com>
2024-02-01 12:11:37 +0545

    change url

--- src/components/News.js
+++ src/components/News.js
@@ -15,7 +15,7 @@ export class News extends Component {
 
 
      async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0b5d88a4262d41e9a7af59ac51563f84&page=1&pageSize=20";
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0b5d88a4262d41e9a7af59ac51563f84&page=1&pageSize=${(this.props.pageSize)}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
@@ -24,7 +24,7 @@ export class News extends Component {
 
       handlePrevClick =async()=>{
         console.log("Previous")
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0b5d88a4262d41e9a7af59ac51563f84&page=${(this.state.page - 1)}&pageSize=20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0b5d88a4262d41e9a7af59ac51563f84&page=${(this.state.page - 1)}&pageSize=${(this.props.pageSize)}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
@@ -37,11 +37,11 @@ export class News extends Component {
       }
       handleNextClick = async()=>{
         console.log("Next")
-        if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
-
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
+          
         }
         else{
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0b5d88a4262d41e9a7af59ac51563f84&page=${(this.state.page + 1)}&pageSize=20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0b5d88a4262d41e9a7af59ac51563f84&page=${(this.state.page + 1)}&pageSize=$(this.props.pageSize)`;
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
@@ -56,7 +56,7 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-4 '>
-        <h1>NewsMonkey - Top Headline</h1>
+        <h1 className='text-center'>NewsMonkey - Top Headline</h1>
         
         <div className="row">
         {this.state.articles.map((element)=>{
@@ -69,7 +69,7 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between">
         <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
 
         </div>
         
